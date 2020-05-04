f43f0eb140b3c6926fc58781dacc15fc9754e2b6
nulzhar <filipe.gmaia@gmail.com>
2020-05-03 23:06:04 -0300

    New test

---
+++ precache-manifest.563d2093560117cb28bef4bd97f34f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a4b2f12661a9fa6c0b5e1e0225363c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "714134c28e859d851bcb",
+    "url": "/static/css/main.7da8013d.chunk.css"
+  },
+  {
+    "revision": "c826cfabdd299c99cd53",
+    "url": "/static/js/2.046d5282.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.046d5282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714134c28e859d851bcb",
+    "url": "/static/js/main.f50c0513.chunk.js"
+  },
+  {
+    "revision": "8a596cbaa34dc805a116",
+    "url": "/static/js/runtime-main.c2fa4b23.js"
+  }
+]);
