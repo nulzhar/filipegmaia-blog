5aa4a81c4c3a5e1700aa6b46bd4b92d190326062
nulzhar <filipe.gmaia@gmail.com>
2020-05-03 23:30:56 -0300

    Updates

---
+++ precache-manifest.9b1b06f8eba92f432940df7bc8a93c4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e34f091daca043254f252c3e2c6853",
+    "url": "/filipegmaia-blog/index.html"
+  },
+  {
+    "revision": "a41eba5f0a6865a58053",
+    "url": "/filipegmaia-blog/static/css/main.7da8013d.chunk.css"
+  },
+  {
+    "revision": "c826cfabdd299c99cd53",
+    "url": "/filipegmaia-blog/static/js/2.046d5282.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filipegmaia-blog/static/js/2.046d5282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41eba5f0a6865a58053",
+    "url": "/filipegmaia-blog/static/js/main.acb15ccd.chunk.js"
+  },
+  {
+    "revision": "6e0e4e427caa24d8d19a",
+    "url": "/filipegmaia-blog/static/js/runtime-main.f41c26d0.js"
+  }
+]);
